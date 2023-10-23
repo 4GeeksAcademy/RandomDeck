cf32b20f225d34544d1f4430c07c94f7ad46192e
navia <naviamatute@yahoo.com>
2023-10-23 19:14:38 -0400

    Final

--- src/app.js
+++ src/app.js
@@ -1,11 +1,27 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+let cardface = [ "A", "2", "3", "4", "5","6","7","8","9","10","11","J","K","Q"];
+let mySuit = [ "♥", "♠", "♦", "♠"];
+let face = cardface[Math.floor(Math.random() * cardface.length)]
+let suit = mySuit[Math.floor(Math.random() * mySuit.length)]
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let container = document.getElementsByClassName('container')
+  let top_left = document.createElement('span')
+  let bottom_right = document.createElement('span')
+  let center = document.createElement('p')
+  
+  top_left.innerText =suit
+  bottom_right.innerText =suit
+  center.innerText = face
+
+  container[0].appendChild(top_left)
+  container[0].appendChild(bottom_right)
+  container[0].appendChild(center)
+
+ if (suit == "♥" || suit == "♦")  {
+  return "red" ;
+  
+ }
+
 };
+ 
