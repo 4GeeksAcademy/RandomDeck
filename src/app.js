0496e15ed841143a910a1dc96a55ddc1dbe1ed2c
navia <naviamatute@yahoo.com>
2023-10-23 20:15:57 -0400

    fixed clover

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 
 let cardface = [ "A", "2", "3", "4", "5","6","7","8","9","10","11","J","K","Q"];
-let mySuit = [ "♥", "♠", "♦", "♠"];
+let mySuit = [ "♥", "♠", "♦", "♣"];
 let face = cardface[Math.floor(Math.random() * cardface.length)]
 let suit = mySuit[Math.floor(Math.random() * mySuit.length)]
 
